7080c98f9b9b0a62ef46b0dc4aa1b896674ce5e8
swatiasnani10 <54753786+swatiasnani10@users.noreply.github.com>
2021-02-23 19:26:27 -0500

    RA week 2
    Dataset and codes for week2.

---
+++ Code/Simulation_02.R
@@ -0,0 +1,41 @@
+#Assumptions for one market and one time period
+
+N_pcp = 100;        #No. of physicians
+N_spe=50;           #No. of specialists
+N_pcp2spe= 5;       #No. of patients sent by a physician to a particular specialist
+N_pat = N_pcp * N_spe * N_pcp2spe   #No. of patients
+
+#Generate a random quality measure for each of the 25k patients
+Patient_id = 1:N_pat                                  #Unique id for each patient
+
+# Gen. phy-spec pairs. Eg.(1,1), (1,2),..(1,50),(2,1). (2,2)....(2,50),...,(100,1),(100,2)..(100,50)
+df_01 = expand.grid(1:N_pcp,1:N_spe)                   #Gen. phy-spec pairs
+nrow(df_01)                                            #This data frame has 5000 rows.
+seq_len(nrow(df_01))                                   #creates a sequence from 1 to 5000 (= #rows in result.df)
+df_01 = df_01[rep(seq_len(nrow(df_01)), each = N_pcp2spe),] #Repeat seq. from 1-5k for 5 times
+Patient_id = 1:N_pat
+df_01$id_patient  = Patient_id                          #Unique id for each patient
+
+# Give row and column names
+rownames(df_01) = 1:nrow(df_01)                        #Name the rows with whole numbers
+colnames(df_01) = c("id_specialist","id_physician", "id_patient")    #Name the columns
+
+
+random.spe = runif(50)     #Draw 50 random numbers uniformly (as we have 50 specialists and we want a random value for each specialist)
+df_01$Specialist_quality = -1
+for(index.spe in 1:N_spe){        #As we are operating on specialist level, loop will be based on specialist number
+  id.current.spe = index.spe      # by our assumption of specialist id
+  df_01$Specialist_quality [df_01$id_specialist == id.current.spe] = random.spe[index.spe]     #We pick up random number from random.spe acc. to the index of specialist
+} #In variable quality.spe, filter those specialist ids that are equal the current specialist id, and assign rv)
+
+                        
+quality.pat = runif(N_pat)           #Generates #N_pat (=25k) uniform random samples b/w 0 and 1
+Patient_outcome = runif(N_pat)  
+df_01$Patient_outcome = Patient_outcome
+
+
+Quality = ifelse(df_01$Patient_outcome > df_01$Specialist_quality , 1, 0)     #If the patient outcome is more than specialist quality, gen 1, o/w 0.
+df_01$Quality = Quality
+
+save(df_01 ,file = "df_02.RData")
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RAproject_01
+
+##The Data folder contains versions of data that I will simulate by each week. For eg. df_01 is the dataset prepared in the first week. df_02 is the data prepared in the second week of this sem.
+
+
+##The Code folder also contains versions of code files. For eg. Simulation_01.R is the code file for the first week, Simulation_01.R is for the second week.
+
+
+#This is to keep record of the changes made every week, in the dataset and codes.
